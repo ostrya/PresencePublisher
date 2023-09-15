ca0a073c7d70df562d2fd1a7f2a8abefbbe555c1
ostrya <ostrya@mailbox.org>
2023-09-15 15:25:13 +0200

    replace deprecated API

--- app/src/main/java/org/ostrya/presencepublisher/mqtt/context/device/LocationProvider.java
+++ app/src/main/java/org/ostrya/presencepublisher/mqtt/context/device/LocationProvider.java
@@ -1,7 +1,8 @@
 package org.ostrya.presencepublisher.mqtt.context.device;
 
+import static android.location.LocationManager.PASSIVE_PROVIDER;
+
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 
@@ -12,7 +13,6 @@ import java.util.Locale;
 
 public class LocationProvider {
     private static final String TAG = "LocationProvider";
-    private static final Criteria NO_CRITERIA = new Criteria();
     // inspired from https://datatracker.ietf.org/doc/html/rfc5870
     private static final String GEO_FORMAT = "geo:%f,%f;u=%f;timestamp=%d";
     private final LocationManager locationManager;
@@ -27,13 +27,7 @@ public class LocationProvider {
             DatabaseLogger.w(TAG, "No location manager available, unable to get location");
             return MessageContext.UNKNOWN;
         }
-        // according to Javadoc, this will give us the provider with best accuracy
-        String provider = locationManager.getBestProvider(NO_CRITERIA, true);
-        if (provider == null) {
-            DatabaseLogger.w(TAG, "Unable to get location provider");
-            return MessageContext.UNKNOWN;
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
+        Location location = locationManager.getLastKnownLocation(PASSIVE_PROVIDER);
         if (location == null) {
             DatabaseLogger.w(TAG, "No last known location found");
             return MessageContext.UNKNOWN;
