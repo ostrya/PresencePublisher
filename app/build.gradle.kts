76afb6127e5ec6b15df926e85aa69b8b392c1c48
ostrya <ostrya@mailbox.org>
2025-04-02 20:38:24 +0200

    update version after release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         multiDexEnabled = true
         targetSdk = 35
         vectorDrawables.useSupportLibrary = true
-        versionCode = 56
+        versionCode = 57
         versionName = "2.6.4"
         javaCompileOptions {
             annotationProcessorOptions {
