e5c292fe29e42ea4151b29f379332422c66d6d70
ostrya <ostrya@mailbox.org>
2025-05-28 07:04:27 +0200

    update dependencies and notice

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -151,7 +151,7 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.work:work-runtime:2.10.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.guava:guava:33.4.6-android")
+    implementation("com.google.guava:guava:33.4.8-android")
     implementation("org.altbeacon:android-beacon-library:2.21.1")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     testImplementation("junit:junit:4.13.2")
